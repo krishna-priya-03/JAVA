0ebc5363f324410b397a1b9dcafbc3c662184989
krishna-priya-03 <kpriye74@gmail.com>
2026-03-02 14:48:08 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class linearsearch
+{
+    public static void main(String args[])
+    {
+        int c,n,search,array[];
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the number of elements");
+        n=in.nextInt();
+        array=new int[n];
+        System.out.println("enter those "+n+" elements");
+        for(c=0;c<n;c++)
+            array[c]=in.nextInt();
+        System.out.println("enter value to find");
+        search=in.nextInt();
+        for(c=0;c<n;c++)
+            if(array[c]==search)
+            {
+               System.out.println(search+" is present at location "+(c+1));
+                break;
+            }
+        if(c==n)
+            System.out.println(search+" is not present in array");
+        }
+    }
